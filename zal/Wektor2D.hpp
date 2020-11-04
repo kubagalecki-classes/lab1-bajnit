cece8827dd8d803aa5dd725da8641860f9646b91
bajnit <krzysiek.kallas@me.com>
2020-11-04 22:58:42 +0000

    wersja bez zbędnych dodatków

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -6,30 +6,22 @@ class Wektor2D
     
     public:
 
-    int populacja(){return num_wek;}
-
     void setX(double a) {x=a;}
     void setY(double a) {y=a;}
     double getX() {return x;}
     double getY() {return y;}
-    void print() { std::cout <<"norma wektora wynosi: "<< norm() << '\n'; }
 
    Wektor2D (double a, double b){
-        ++num_wek;
         x = a;
         y = b;
-        std::cout << "x-> " << x << " y-> " <<  y << "\n";
     }
 
   Wektor2D() : x(0), y(0)
-    {
-        ++num_wek;
-        
+    { 
     }
 
     ~Wektor2D(){
-        --num_wek;
-       // std::cout << "Destruktor"<<"\n";
+
     }
 
    
@@ -38,16 +30,9 @@ class Wektor2D
     double x;
     double y;
 
-    static int num_wek;
-       
-    double norm() {return sqrt((x*x)+(y*y)); }; 
-
     friend Wektor2D operator+(const Wektor2D& v_1, const Wektor2D& v_2);   
-    friend Wektor2D operator*(double scalar,const Wektor2D& v_1);
-    //friend std::ostream& operator<<(std::ostream& os, Wektor2D& v_1);        
+    friend Wektor2D operator*(double scalar,const Wektor2D& v_1);       
 };
-
-int Wektor2D::num_wek = 0; // inizjaclizajca licznika zerem
 
 
 Wektor2D operator+(Wektor2D& v_1, Wektor2D& v_2){
